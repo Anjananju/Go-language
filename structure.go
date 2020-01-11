f8c5fa48ba8889a62d75471c4025cfcac0718f9d
Anjana P Nair <32244973+Anjananju@users.noreply.github.com>
2020-01-12 02:29:47 +0530

    Update structure.go

--- structure.go
+++ structure.go
@@ -10,6 +10,13 @@ type student struct
   {
   student1:=student("Anjana",23,"Female");
   fmt.println(student1);
+    fmt.println("Name is",student1.name);//to access single element
+    //to set a new value to age
+    student1.age=24;
+    //to print new age value only
+    fmt.println("age is",student1.age);
+    //to print structure with name-anjana,age=24,gender female
+    fmt.println(student1);
   var student2 student; //declaring variable and it is initialised all structure elements as 0
    fmt.println(student2);
   }
